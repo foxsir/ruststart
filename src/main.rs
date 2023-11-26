ef2addfdc3666c4c81e46d37c3fdc350244178c4
foxsir <contact@umbst.com>
2023-11-26 20:04:31 +0800

    update code

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ mod syntax;
 
 // use syntax::libs::hosting;
 // use syntax::guess_game::guess;
-// use syntax::variables::guess;
+// use syntax::data_types::run;
+use syntax::functions::run;
 
 fn main() {
-
+    run();
 }
--- src/syntax.rs
+++ src/syntax.rs
@@ -1,2 +1,5 @@
-pub mod libs;
-pub mod guess_game;
+// pub mod libs;
+// pub mod guess_game;
+// pub mod variables;
+// pub mod data_types;
+pub mod functions;
